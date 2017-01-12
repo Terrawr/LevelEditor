830653a948de967063b5212acee2abd7cdf107ec
JohannesLüke <lueke.johannes@gmail.com>
2017-01-12 14:21:38 +0100

    added comment

--- LevelEditorCore/UserInterface.h
+++ LevelEditorCore/UserInterface.h
@@ -1,1 +1,14 @@
 #pragma once
+
+/*
+Contains all functions and structs necessary for our
+internal User interface.
+
+What needs doing here:
+TODO(jojo):
++Implementing Buttons
++Implementing TextInputBoxes
++Implementing TextOutputBoxes
++Implementing PictureBox
++Implementing ...
+*/
